4082461cffe45747c2a4a90ba3b88ee4358e58d1
Arjuna-Dev <arjuna1@protonmail.com>
2024-06-07 09:54:25 +0200

    Converting text to class and hardcoded working example

--- aider/sendchat.py
+++ aider/sendchat.py
@@ -8,6 +8,53 @@ import openai
 from aider.dump import dump  # noqa: F401
 from aider.litellm import litellm
 
+from dataclasses import dataclass
+from typing import List
+import time
+
+@dataclass
+class Usage:
+    completion_tokens: int
+    prompt_tokens: int
+    total_tokens: int
+
+@dataclass
+class Message:
+    content: str
+    role: str
+
+@dataclass
+class Choices:
+    finish_reason: str
+    index: int
+    message: Message
+
+@dataclass
+class ModelResponse:
+    id: str
+    choices: List[Choices]
+    created: int
+    model: str
+    object: str
+    system_fingerprint: str
+    usage: Usage
+
+def rectify_text(content: str) -> ModelResponse:
+    message = Message(content=content, role='assistant')
+    choice = Choices(finish_reason='stop', index=0, message=message)
+    usage = Usage(completion_tokens=37, prompt_tokens=1576, total_tokens=1613)
+    model_response = ModelResponse(
+        id='chatcmpl-9XEpc5YaNIltTVcv1sxmbsLJi1ZO7',
+        choices=[choice],
+        created=int(time.time()),
+        model='gpt-4o-2024-05-13',
+        object='chat.completion',
+        system_fingerprint='fp_319be4768e',
+        usage=usage
+    )
+    return model_response
+
+
 # from diskcache import Cache
 
 
@@ -67,8 +114,16 @@ def send_with_retries(model_name, messages, functions, stream, temperature=0):
         return hash_object, CACHE[key]
 
     # del kwargs['stream']
+    # sentinel = "END"
+    # res = ""
+    # for line in iter(input, sentinel):
+    #     res += line + "\n"
+    # res = input("Paste your chatGPT response here: ")
 
-    res = litellm.completion(**kwargs)
+    res = rectify_text('Documents/mydocs/Programming/aider_test/main.py\n```python\n<<<<<<< SEARCH\nfor i in range(5):\n=======\nfor i in range(10):\n>>>>>>> REPLACE\n```')
+
+    # res = litellm.completion(**kwargs)
+    print('resresres: ', res)
 
     if not stream and CACHE is not None:
         CACHE[key] = res
